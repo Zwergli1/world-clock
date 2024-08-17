6929cd90ec238e4ba01874f20db9a9b90021bd6c
Zwergli1 <54478681+Zwergli1@users.noreply.github.com>
2024-08-17 20:06:24 +0200

    Added momentjs with timezone

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,17 @@
 let losAngelesElement = document.querySelector("#los-angeles");
 let losAngelesDateElement = losAngelesElement.querySelector(".date");
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-let losAngelesTime = moment();
+let losAngelesTime = moment().tz("America/Los_Angeles");
 
 losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-losAngelesTimeElement.innerHTML = `${losAngelesTime.format(
-  "h:mm:ss"
-)} <small>${losAngelesTime.format("A")}</small>`;
+losAngelesTimeElement.innerHTML = losAngelesTime.format(
+  "h:mm:ss [<small>]A[</small>]"
+);
+
+let zurichElement = document.querySelector("#zurich");
+let zurichDateElement = zurichElement.querySelector(".date");
+let zurichTimeElement = zurichElement.querySelector(".time");
+let zurichTime = moment().tz("Europe/Zurich");
+
+zurichDateElement.innerHTML = zurichTime.format("MMMM Do YYYY");
+zurichTimeElement.innerHTML = zurichTime.format("h:mm:ss [<small>]A[</small>]");
