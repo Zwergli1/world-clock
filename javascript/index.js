2848700279e9c0b8bef9d26e877a3bc86b0d7051
Zwergli1 <54478681+Zwergli1@users.noreply.github.com>
2024-08-17 20:16:53 +0200

    Added auto updates

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,23 @@
-let losAngelesElement = document.querySelector("#los-angeles");
-let losAngelesDateElement = losAngelesElement.querySelector(".date");
-let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-let losAngelesTime = moment().tz("America/Los_Angeles");
+function updateTime() {
+  let sanFranciscoElement = document.querySelector("#san-francisco");
+  let sanFranciscoDateElement = sanFranciscoElement.querySelector(".date");
+  let sanFranciscoTimeElement = sanFranciscoElement.querySelector(".time");
+  let sanFranciscoTime = moment().tz("America/Los_Angeles");
 
-losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-losAngelesTimeElement.innerHTML = losAngelesTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+  sanFranciscoDateElement.innerHTML = sanFranciscoTime.format("MMMM Do YYYY");
+  sanFranciscoTimeElement.innerHTML = sanFranciscoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
-let zurichElement = document.querySelector("#zurich");
-let zurichDateElement = zurichElement.querySelector(".date");
-let zurichTimeElement = zurichElement.querySelector(".time");
-let zurichTime = moment().tz("Europe/Zurich");
+  let zurichElement = document.querySelector("#zurich");
+  let zurichDateElement = zurichElement.querySelector(".date");
+  let zurichTimeElement = zurichElement.querySelector(".time");
+  let zurichTime = moment().tz("Europe/Zurich");
 
-zurichDateElement.innerHTML = zurichTime.format("MMMM Do YYYY");
-zurichTimeElement.innerHTML = zurichTime.format("h:mm:ss [<small>]A[</small>]");
+  zurichDateElement.innerHTML = zurichTime.format("MMMM Do YYYY");
+  zurichTimeElement.innerHTML = zurichTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
